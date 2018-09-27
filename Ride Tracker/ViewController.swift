51f311e956dab208ee1e542f9f159c4f90bade3c
Gabe Revells <gcrevell@mtu.edu>
2018-09-27 09:08:49 -0400

    Initial Commit

---
+++ Ride Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ride Tracker
+//
+//  Created by Gabriel Revells on 9/27/18.
+//  Copyright © 2018 Gabriel Revells. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Ride TrackerTests/Ride_TrackerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Ride_TrackerTests.swift
+//  Ride TrackerTests
+//
+//  Created by Gabriel Revells on 9/27/18.
+//  Copyright © 2018 Gabriel Revells. All rights reserved.
+//
+
+import XCTest
+@testable import Ride_Tracker
+
+class Ride_TrackerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
